64d09da074dc5f74307ba950f8455a48548d08c2
joseph-charbel <josephcharbel3@gmail.com>
2026-05-10 21:53:47 +1000

    moved assets

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import yaml from '@modyfi/vite-plugin-yaml'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [yaml()],
+  publicDir: false,
+  plugins: [
+    yaml(),
+    ...viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/**/*',
+          dest: 'assets',
+          rename: { stripBase: 2 },
+        },
+      ],
+    }),
+  ],
   define: {
     ENGINE_DEBUG: JSON.stringify(true),
   },
